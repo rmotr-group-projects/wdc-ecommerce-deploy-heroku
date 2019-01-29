4dc0d28ec7baa561f3e541806b70919dbae3aa25
Ivan Zugnoni <zugnoni.ivan@gmail.com>
2019-01-29 12:28:06 -0300

    Update settings

--- ecommerce/ecommerce/settings/base.py
+++ ecommerce/ecommerce/settings/base.py
@@ -14,9 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DJANGO_PROJECT_DIR = os.path.dirname(BASE_DIR)
-PROJECT_DIR = os.path.dirname(DJANGO_PROJECT_DIR)
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -128,10 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, "static"),
-]
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
