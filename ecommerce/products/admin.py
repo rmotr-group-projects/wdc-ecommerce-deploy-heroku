2a1b142286c6619f057fce351a9ee4dc1c82e68d
Ivan Zugnoni <zugnoni.ivan@gmail.com>
2019-01-30 14:25:34 -0300

    Add products models to admin

--- ecommerce/products/admin.py
+++ ecommerce/products/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from products.models import Product, Category, ProductImage
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    pass
