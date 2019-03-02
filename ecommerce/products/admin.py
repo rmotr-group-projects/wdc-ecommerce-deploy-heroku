feed4392152bd6b670cfb614de0edcadf34486c6
Poom Khor <poom.khor@gmail.com>
2019-03-02 02:27:58 +0000

    'update-procfile'

--- ecommerce/products/admin.py
+++ ecommerce/products/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from products.models import Product, Category, ProductImage
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    pass
