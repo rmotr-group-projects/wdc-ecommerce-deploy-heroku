c5982ff0dae125827ea8f3090ca956f40314a699
Jonathan Flowers <jmflowers134@gmail.com>
2019-04-26 16:53:43 -0400

    Update prod.py

--- ecommerce/ecommerce/settings/prod.py
+++ ecommerce/ecommerce/settings/prod.py
@@ -9,4 +9,7 @@ SECRET_KEY = os.getenv(
     '^8ai-6gb!yyg19uangdahsi8a%c=)mb0xler7%0klh1mz!^snago;91_')
 
 
-# YOUR CODE HERE
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
