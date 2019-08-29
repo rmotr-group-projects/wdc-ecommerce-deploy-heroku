9e55ca9f8a07c7f806a8428a72cb5923e9fba88c
JPAS <jordan.pa.schultz@gmail.com>
2019-08-28 19:57:25 -0700

    Made some changes

--- ecommerce/ecommerce/settings/prod.py
+++ ecommerce/ecommerce/settings/prod.py
@@ -3,10 +3,11 @@ import dj_database_url
 from .base import *
 
 
-ALLOWED_HOSTS = ['*']
 SECRET_KEY = os.getenv(
     'DJANGO_SECRET_KEY',
     '^8ai-6gb!yyg19uangdahsi8a%c=)mb0xler7%0klh1mz!^snago;91_')
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
-
-# YOUR CODE HERE
+ALLOWED_HOSTS = ['*']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
